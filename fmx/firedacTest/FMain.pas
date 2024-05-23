02b7b5b445075448a9e52022cd84f898449ab5ce
tonim <toni@reportman.es>
2024-05-23 12:20:06 +0200

    Update Zeos drivers, test firedac

--- fmx/firedacTest/FMain.pas
+++ fmx/firedacTest/FMain.pas
@@ -28,8 +28,9 @@ uses
   FireDAC.Phys.Oracle,
   FireDAC.Phys.SQLite, FireDAC.Phys.IB, FireDAC.Phys.PG,
   FireDAC.Phys.IBBase, FireDAC.Phys.FB, FireDAC.Phys.ADS, FireDAC.Phys.MySQL,
-  FireDAC.Phys.ODBCBase;
-
+  FireDAC.Phys.ODBCBase, ZAbstractDataset, ZMemTable, ZDataset,
+  ZAbstractRODataset, ZAbstractConnection, ZTransaction, ZConnection, ZDatasetUtils,
+  ZDbcIntfs;
 type
   TForm1 = class(TForm)
     Label1: TLabel;
@@ -63,11 +64,23 @@ type
     FDMemTable1: TFDMemTable;
     Label3: TLabel;
     CheckLoginPrompt: TCheckBox;
+    ZConnection1: TZConnection;
+    ZTransaction1: TZTransaction;
+    ZReadOnlyQuery1: TZReadOnlyQuery;
+    ZMemTable1: TZMemTable;
+    LabelZeosProtocol: TLabel;
+    Label5: TLabel;
+    ComboZeosProtocol: TComboBox;
+    Panel3: TPanel;
+    ButtonDisconnect: TButton;
     procedure ButtonConnectClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure ComboDriverChange(Sender: TObject);
+    procedure ButtonDisconnectClick(Sender: TObject);
   private
     { Private declarations }
+    isZeosLib: boolean;
   public
     { Public declarations }
   end;
@@ -80,39 +93,136 @@ implementation
 {$R *.fmx}
 
 procedure TForm1.Button1Click(Sender: TObject);
+var nombreCliente:string;
 begin
- if (not FDConnection1.Connected) then
+ if (isZeosLib) then
  begin
-   raise Exception.Create('Go to Connection tab and connect database before opening the dataset.');
- end;
- FDQuery1.Close;
- FDQuery1.SQL.Text := '';
- FDQuery1.Params.Clear;
- FDQuery1.ResourceOptions.EscapeExpand :=   CheckExpandParams.IsChecked;
- FDQuery1.ResourceOptions.ParamCreate :=   CheckCreateParams.IsChecked;
- FDQuery1.ResourceOptions.ParamExpand :=   CheckExpandParams.IsChecked;
- FDQuery1.ResourceOptions.UnifyParams :=   CheckUnifyParams.IsChecked;
- FDQuery1.ResourceOptions.MacroCreate :=   CheckCreateMacros.IsChecked;
- FDQuery1.ResourceOptions.MacroExpand :=   CheckExpandMacros.IsChecked;
- FDQuery1.SQL.Text := MemoSQL.Text;
- FDQuery1.Active:=true;
- try
-  FDMemTable1.CloneCursor(FDQuery1, False, False);
- finally
-  FDQuery1.Active:=false;
+   if (not ZConnection1.Connected) then
+   begin
+     raise Exception.Create('Go to Connection tab and connect database before opening the dataset.');
+   end;
+   ZReadOnlyQuery1.Close;
+   ZReadOnlyQuery1.SQL.Text := '';
+   ZReadOnlyQuery1.Params.Clear;
+   //ZReadOnlyQuery1.ResourceOptions.EscapeExpand :=   CheckExpandParams.IsChecked;
+   ZReadOnlyQuery1.ParamCheck :=   CheckCreateParams.IsChecked;
+   //ZReadOnlyQuery1.ResourceOptions.ParamExpand :=   CheckExpandParams.IsChecked;
+   //ZReadOnlyQuery1.ResourceOptions.UnifyParams :=   CheckUnifyParams.IsChecked;
+   //ZReadOnlyQuery1.ResourceOptions.MacroCreate :=   CheckCreateMacros.IsChecked;
+   //ZReadOnlyQuery1.ResourceOptions.MacroExpand :=   CheckExpandMacros.IsChecked;
+   ZReadOnlyQuery1.SQL.Text := MemoSQL.Text;
+   ZReadOnlyQuery1.Active:=true;
+   // ZMemTable convierte las columnas widestring a string perdiendo información
+(*   try
+    ZMemTable1.CloneDataFrom(ZReadOnlyQuery1, true);
+    
+          
+   finally
+    ZReadOnlyQuery1.Active:=false;
+   end;*)
+   DataSource1.DataSet:=ZReadOnlyQuery1;
+ end
+ else
+ begin
+   DataSource1.DataSet:=nil;
+   if (not FDConnection1.Connected) then
+   begin
+     raise Exception.Create('Go to Connection tab and connect database before opening the dataset.');
+   end;
+   FDQuery1.Close;
+   FDQuery1.SQL.Text := '';
+   FDQuery1.Params.Clear;
+   FDQuery1.ResourceOptions.EscapeExpand :=   CheckExpandParams.IsChecked;
+   FDQuery1.ResourceOptions.ParamCreate :=   CheckCreateParams.IsChecked;
+   FDQuery1.ResourceOptions.ParamExpand :=   CheckExpandParams.IsChecked;
+   FDQuery1.ResourceOptions.UnifyParams :=   CheckUnifyParams.IsChecked;
+   FDQuery1.ResourceOptions.MacroCreate :=   CheckCreateMacros.IsChecked;
+   FDQuery1.ResourceOptions.MacroExpand :=   CheckExpandMacros.IsChecked;
+   FDQuery1.SQL.Text := MemoSQL.Text;
+   //FDquery1.FetchOptions.Mode:=TFDFetchMode.fmAll;
+   FDQuery1.Active:=true;
+   try
+    //FDQuery1.Last;
+    //FDMemTable1.CloneCursor(FDQuery1, False, False);    
+   finally
+    //FDQuery1.Active:=false;
+   end;
+  // DataSource1.DataSet:=FDMemTable1;
+   DataSource1.DataSet:=FDQuery1;
  end;
  TabControl1.ActiveTab := TabItem3;
 end;
 
 procedure TForm1.ButtonConnectClick(Sender: TObject);
+var
+ driverName:string;
+ param:String;
 begin
- FDConnection1.DriverName:=ComboDriver.Items[ComboDriver.ItemIndex];
- FDConnection1.Params.Text := MemoParams.Text;
- FDConnection1.LoginPrompt := CheckLoginPrompt.IsChecked;
- FDConnection1.Params.AddPair('DriverId',ComboDriver.Items[ComboDriver.ItemIndex]);
- FDConnection1.Close;
- FDConnection1.Open;
+ driverName:=ComboDriver.Items[ComboDriver.ItemIndex];
+ if (driverName = 'Zeos') then
+ begin
+  isZeosLib:=true;
+  ZConnection1.Protocol:=ComboZeosProtocol.Items[ComboZeosProtocol.ItemIndex];
+  ZConnection1.Database:=MemoParams.Lines.Values['Database'];
+  if (ZConnection1.Database = '') then
+  begin
+   ZConnection1.Database:=MemoParams.Lines.Values['DataSource'];
+  end;
+  ZConnection1.User:=MemoParams.Lines.Values['User_Name'];
+  ZConnection1.Password:=MemoParams.Lines.Values['Password'];
+  //ZConnection1.Properties.Clear;
+  //ZConnection1.Properties.Add('codepage=utf8');
+  //ZConnection1.ControlsCodePage:=cCP_UTF16;
+  //ZConnection1.RawCharacterTransliterateOptions.Params:=true;
+  //ZConnection1.RawCharacterTransliterateOptions.Encoding:=encUTF8;
+  //ZConnection1.ClientCodepage:=;
+  ZConnection1.Connect;
+ end
+ else
+ begin
+  isZeosLib:=false;
+  FDConnection1.DriverName:=ComboDriver.Items[ComboDriver.ItemIndex];
+  FDConnection1.Params.Text := MemoParams.Text;
+  FDConnection1.LoginPrompt := CheckLoginPrompt.IsChecked;
+  FDConnection1.Params.AddPair('DriverId',driverName);
+  FDConnection1.Close;
+  FDConnection1.Open;
+ end;
+ ButtonConnect.Enabled:=false;
+ ButtonDisconnect.Enabled:=true;
+
  TabControl1.ActiveTab := TabItem2;
+end;
+
+procedure TForm1.ButtonDisconnectClick(Sender: TObject);
+begin
+ if (isZeosLib) then
+ begin
+  ZConnection1.Disconnect;
+ end
+ else
+ begin
+   FDConnection1.Close;
+ end;
+ ButtonConnect.Enabled:=true;
+ BUttonDisconnect.Enabled:=false;
+end;
+
+procedure TForm1.ComboDriverChange(Sender: TObject);
+var
+ driverName:String;
+begin
+ driverName:=ComboDriver.Items[ComboDriver.ItemIndex];
+ if (driverName = 'Zeos') then
+ begin
+   LabelZeosProtocol.Enabled:=true;
+   ComboZeosProtocol.Enabled:=true;
+ end
+ else
+ begin
+   LabelZeosProtocol.Enabled:=false;
+   ComboZeosProtocol.Enabled:=false;
+ end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -120,7 +230,7 @@ begin
  TabControl1.ActiveTab:=TabControl1.Tabs[0];
  ComboDriver.ItemIndex := 12;
  MemoParams.Text := FDConnection1.Params.Text;
-
+ ComboZeosProtocol.ItemIndex:=ComboZeosProtocol.Items.IndexOf('odbc_w');
 end;
 
 end.
